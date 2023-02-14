27515f7ca8cf6d8aa1bad9dc0b03290ea8d6e14e
0dminnimda <0dminnimda@gmail.com>
2023-02-14 23:31:32 +0300

    dsa\lab2\memo.c: include missed stuff and fix pointer addition

--- dsa/lab2/memo.c
+++ dsa/lab2/memo.c
@@ -1,8 +1,10 @@
 #include <stdlib.h>
+#include <string.h>
+
 
 void *nulled_realloc(void *ptr, size_t old_size, size_t new_size) {
     ptr = realloc(ptr, new_size);
     if (ptr != NULL && new_size > old_size)
-        memset(ptr + old_size, 0, new_size - old_size);
+        memset((char *)ptr + old_size, 0, new_size - old_size);
     return ptr;
 }
