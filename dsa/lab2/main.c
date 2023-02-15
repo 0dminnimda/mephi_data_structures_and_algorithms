0c3f2526b93ba047fb1723e0bb6c7dfab83fafe9
0dminnimda <0dminnimda@gmail.com>
2023-02-15 14:37:16 +0300

    dsa\lab2: include queue helpers into queue.h

--- dsa/lab2/main.c
+++ dsa/lab2/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "helpers.h"
 #include "queue.h"
 
 int main() {
--- dsa/lab2/queue.h
+++ dsa/lab2/queue.h
@@ -1,5 +1,5 @@
 #include "error.h"
-#include "queue.h"
+#include "queue_decl.h"
 
 #define default_queue(queue) WITH_ERROR(default_queue(queue))
 #define queue_push(queue, value) WITH_ERROR(queue_push(queue, value))
