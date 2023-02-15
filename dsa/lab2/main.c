2215dba8450739a0c806bdfe93e48f0e434c6d1e
0dminnimda <0dminnimda@gmail.com>
2023-02-15 14:27:33 +0300

    dsa\lab2\main.c: make a push test

--- dsa/lab2/main.c
+++ dsa/lab2/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "helpers.h"
+#include "queue.h"
+
 int main() {
-    printf("Hello world!\n");
+    Queue *q = NULL;
+    default_queue(&q);
+
+    while (1) {
+        int x;
+        scanf("%d", &x);
+        if (x == 0) break;
+        queue_push(q, x);
+    }
+
+    while (queue_size(q)) {
+        printf("%d ", queue_front(q));
+        queue_pop(q);
+    }
+    printf("\n");
+
+    destroy_queue(q);
 }
