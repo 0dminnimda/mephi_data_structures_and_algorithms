f0875803eebce8f5fb3ae89db9331be619e1a26a
0dminnimda <0dminnimda@gmail.com>
2023-02-15 16:21:07 +0300

    dsa\lab2\queue.c: handle wrong values of QUEUE_IMPL

--- dsa/lab2/queue.c
+++ dsa/lab2/queue.c
@@ -1,5 +1,7 @@
-#if QUEUE_IMP == 'L'
+#if !defined(QUEUE_IMPL) || (QUEUE_IMPL == 'L')
 #include "queue_list_impl.c"
-#else
+#elif (QUEUE_IMPL == 'V')
 #include "queue_vector_impl.c"
+#else
+#error Unknown QUEUE_IMPL value, should be 'V' or 'L'
 #endif
