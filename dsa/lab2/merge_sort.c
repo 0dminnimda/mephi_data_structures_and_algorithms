b7abc0deda54312089aa44d07fcc0369ed303618
0dminnimda <0dminnimda@gmail.com>
2023-02-16 21:57:33 +0300

    dsa\lab2\merge_sort.c: sort takes only length & shifted pointer

--- dsa/lab2/merge_sort.c
+++ dsa/lab2/merge_sort.c
@@ -8,17 +8,17 @@
 
 typedef int (*comparator)(const void *, const void *);
 
-static void sort(void *ptr, void *buff, size_t lo, size_t hi, size_t size,
+static void sort(void *ptr, void *buff, size_t len, size_t size,
                  comparator comp) {
-    if (lo >= hi - 1) return;
+    if (len <= 1) return;
 
-    size_t mid = (lo + hi) / 2;
-    sort(ptr, buff, lo, mid, size, comp);
-    sort(ptr, buff, mid, hi, size, comp);
+    size_t mid = len / 2;
+    sort(ptr, buff, mid, size, comp);
+    sort(add(ptr, mid), add(buff, mid), len - mid, size, comp);
 
-    void *lhs = add(ptr, lo), *lhs_end = add(ptr, mid);
-    void *rhs = add(ptr, mid), *rhs_end = add(ptr, hi);
-    void *last = add(buff, lo);
+    void *lhs = ptr, *lhs_end = add(ptr, mid);
+    void *rhs = lhs_end, *rhs_end = add(ptr, len);
+    void *last = buff;
 
     while (lhs < lhs_end && rhs < rhs_end) {
         if (comp(lhs, rhs) <= 0) {
@@ -31,15 +31,15 @@ static void sort(void *ptr, void *buff, size_t lo, size_t hi, size_t size,
         inc(&last);
     }
 
-    size_t sorted = distance(add(buff, lo), last);
-    memcpy(add(ptr, lo + sorted), lhs, distance(lhs, lhs_end) * size);
-    memcpy(add(ptr, lo), add(buff, lo), sorted * size);
+    size_t sorted = distance(buff, last);
+    memcpy(add(ptr, sorted), lhs, distance(lhs, lhs_end) * size);
+    memcpy(ptr, buff, sorted * size);
     // what's left in rhs (if any) is already in ptr, no need to copy it around
 }
 
-error_t merge_sort(void *ptr, size_t length, size_t size, comparator comp) {
+error_t merge_sort(void *ptr, size_t length, size_t size, comparator compare) {
     void *buffer = NEW(buffer, length * size);
-    sort(ptr, buffer, 0, length, size, comp);
+    sort(ptr, buffer, length, size, compare);
     free(buffer);
     return 0;
 }
