a47efeeade89f841a3b5f3acc2998aded4b5b41f
0dminnimda <0dminnimda@gmail.com>
2023-02-23 18:02:41 +0300

    dsa\lab2\memo.h: NEW_ON_ERROR -> NEW_OR_FAIL

--- dsa/lab2/memo.h
+++ dsa/lab2/memo.h
@@ -6,12 +6,12 @@
 #define MEMORY_ERROR_TYPE MAKE_ERROR_TYPE("MemoryError")
 #define MEMORY_ERROR(message) MAKE_ERROR(MEMORY_ERROR_TYPE, message)
 
-#define NEW_ON_ERROR(var, size)         \
+#define NEW_OR_FAIL(var, size)         \
     (var = calloc(size, sizeof(char))); \
     if ((size) && var == NULL)
 
 #define NEW(var, size) \
-    NEW_ON_ERROR(var, size) return MEMORY_ERROR("Bad allocation");
+    NEW_OR_FAIL(var, size) return MEMORY_ERROR("Bad allocation");
 
 #if IS_PLATFORM(WINDOWS) || IS_PLATFORM(APPLE) || IS_PLATFORM(LINUX)
 size_t memory_size(const void *m);
@@ -21,9 +21,9 @@ size_t memory_size(const void *m);
 
 void *nulled_realloc(void *ptr, size_t old_size, size_t new_size);
 
-#define RENEW_ON_ERROR(var, size)                        \
+#define RENEW_OR_FAIL(var, size)                        \
     (var = nulled_realloc(var, memory_size(var), size)); \
     if ((size) && var == NULL)
 
 #define RENEW(var, size) \
-    RENEW_ON_ERROR(var, size) return MEMORY_ERROR("Bad reallocation");
+    RENEW_OR_FAIL(var, size) return MEMORY_ERROR("Bad reallocation");
