657be56fa0c859ecab8d7bbb83507a7824e97eeb
0dminnimda <0dminnimda@gmail.com>
2023-02-24 05:46:29 +0300

    dsa\lab2\build.sh: update and add sugar_test

--- dsa/lab2/build.sh
+++ dsa/lab2/build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 MY_CC="cc"
-ARGS="input.c merge_sort.c passenger.c load_balancer.c queue.c memo.c main.c -o main.out -O3 -g"
+ARGS="input.c passenger.c load_balancer.c queue.c memo.c main.c -o main.out -O3 -g"
+# ARGS="input.c memo.c sugar_test.c -o main.out -O3 -g"
 
 if [[ $(which cc &> /dev/null; echo $?) != "0" ]]; then
     echo "I did not find 'cc', so using 'clang' instead"
