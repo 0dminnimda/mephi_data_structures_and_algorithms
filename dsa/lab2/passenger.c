a1f631e793977cfe87febe1010bdd12a3b2b8bca
0dminnimda <0dminnimda@gmail.com>
2023-02-24 04:53:17 +0300

    dsa\lab2\passenger.c: allocate name mem in the last place

--- dsa/lab2/passenger.c
+++ dsa/lab2/passenger.c
@@ -14,7 +14,7 @@
 Error parse_passenger(char **str, Passenger *passenger) {
     Passenger p = {0};
 
-    *str += strspn(*str, " ");
+    char *name = *str = *str + strspn(*str, " ");
 
     char *arrival_time = strchr(*str, '/');
     if (arrival_time++ == NULL) return INPUT_ERROR("No arrival time specified");
@@ -22,20 +22,18 @@ Error parse_passenger(char **str, Passenger *passenger) {
     char *service_time = strchr(arrival_time, '/');
     if (service_time++ == NULL) return INPUT_ERROR("No service time specified");
 
-    NEW(p.name, arrival_time - *str);
-    strncpy(p.name, *str, arrival_time - *str);
-
     p.arrival_time = strtol(arrival_time, str, 10);
     if (p.arrival_time < 0 || *str == arrival_time || errno != 0) {
-        DELETE(p.name);
         return INPUT_ERROR("Arrival time is not a valid non-negative number");
     }
 
     p.service_time = strtol(service_time, str, 10);
     if (p.service_time < 0 || *str == service_time || errno != 0) {
-        DELETE(p.name);
         return INPUT_ERROR("Service time is not a valid non-negative number");
     }
+
+    NEW(p.name, arrival_time - name);
+    strncpy(p.name, name, arrival_time - name);
 
     *passenger = p;
     return OK;
