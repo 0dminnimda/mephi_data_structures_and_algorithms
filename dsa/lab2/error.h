e166cf733b9c27ff9327d1d36e2d1e8fb99368fc
0dminnimda <0dminnimda@gmail.com>
2023-02-24 04:44:55 +0300

    dsa\lab2\error.h: update the TRY, add CATCH and THROW

--- dsa/lab2/error.h
+++ dsa/lab2/error.h
@@ -25,23 +25,32 @@ typedef struct {
 #define NOT_AN_ERROR MAKE_ERROR(NULL, NULL)
 #define OK NOT_AN_ERROR
 
-#define ON_ERROR(expr, on_error) \
-    do {                         \
-        Error error = expr;      \
-        if (IS_ERROR(error)) {   \
-            on_error;            \
-        }                        \
-    } while (0);
 
-#define WITH_ERROR(expr) ON_ERROR(expr, return error)
-
-#define TRY(expr) WITH_ERROR(expr)
 #define THROW_ERRORS(expr)           \
     ON_ERROR(expr, {                 \
         FPRINT_ERROR(stderr, error); \
         return -1;                   \
     })
 
-#define CALL(func, ...) WITH_ERROR(func(__VA_ARGS__))
+#define THROW(error) return error;
+
+#define UNIQUE(name) m__unique__variable__name__0123456789__##name##__
+
+#define BASE_TRY(expr) for (Error error = (expr); IS_ERROR(error); error = OK)
+
+#define NO_THROW_TRY(expr)                                 \
+    BASE_TRY(expr)                                         \
+    for (int UNIQUE(error_i); IS_ERROR(error); error = OK) \
+        if (0) 0;
+
+#define TRY(expr)                                        \
+    BASE_TRY(expr)                                       \
+    for (int UNIQUE(error_i) = 1; UNIQUE(error_i) != 2;) \
+        if (UNIQUE(error_i)-- == 0) THROW(error);
+
+#define CATCH_IF(condition) else if ((condition) && (UNIQUE(error_i) = 2))
+#define CATCH(target_error) CATCH_IF(ARE_EQUAL_ERRORS(error, target_error))
+#define CATCH_ALL else
+#define CATCH_N_THROW CATCH_ALL THROW(error)
 
 #endif  // ERROR_H_
