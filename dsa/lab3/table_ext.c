aa4411ff54fa29d01c3ebbc980f0736e53190832
0dminnimda <0dminnimda@gmail.com>
2023-04-07 02:15:58 +0300

    dsa\lab3\table_ext.c: remove old comments

--- dsa/lab3/table_ext.c
+++ dsa/lab3/table_ext.c
@@ -1,70 +1,8 @@
-/*
-Write program in C++, but leave the actual implementation blank (aka make a skeleton):
-
-- Library for vector-based table
-- Console application to work with library table interactively
-
-Library implementations:
-
-1. In-memory
-2. External memory (binary file)
-
-Vector-based table:
-
-`table.h`:
-```c
-typedef unsigned int KeyType;
-typedef unsigned int InfoType;
-typedef size_t IndexType;
-
-struct Item {
-    InfoType *info; // actual data
-    KeyType key; // backreference to KeySpace
-};
-
-struct KeySpace {
-    KeyType key;
-    KeyType par; // nullable
-    Item *info;
-};
-
-struct Table {
-    KeySpace *ks;
-    IndexType msize; // max key space size
-};
-```
-
-No dup keys/must have parent key or null.
-Ordered by parent key. Parent key may duplicate.
-
-Operations:
-
-- Insert element with unique key
-- Delete element by key
-- Search by key (returns an iterator of matches)
-- Output to standard stream
-- Import from text file
-- Remove by key if no parent keys in table = key
-- Search by parent key value, return new table of matches
-
-Use buffering and caching for external memory
-
-Requirements:
-
-- No globals
-- If input incorrect, show error, then continue
-- If error in table op, show it, then continue
-- Modified struct for external memory (add length/offset)
-- Use `fread`/`fwrite` for files (with length)
-- Correct memory usage
-*/
-
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
 
-// #include "common/input.h"
 #include "file_holder.h"
 #include "table.h"
 
