f55f099175a985310ea0f535407e29869c3310cf
0dminnimda <0dminnimda@gmail.com>
2023-04-25 13:09:55 +0300

    dsa/lab3cd: make cmd interface better

--- dsa/lab3cd/main.c
+++ dsa/lab3cd/main.c
@@ -4,50 +4,30 @@
 
 int main() {
     Table *table = create_table(10);
-    printf("Enter command (i/d/s/o/f/a/q):\n");
 
     bool run = true;
     while (run) {
+        printf("Enter command (inp/del/delall/find/out/file/quit):\n");
         printf("> ");
         char *input = read_line();
-        if (strcmp(input, "q") == 0) {
-            run = false;
-        } else if (strcmp(input, "i") == 0) {
+        if (strcmp(input, "inp") == 0) {
             KeyType key;
             Item info;
             printf("Enter key and info:\n");
             scanf("%u %u", &key, &info);
             insert(table, key, info);
-        } else if (strcmp(input, "d") == 0) {
+        } else if (strcmp(input, "del") == 0) {
             KeyType key;
             RelType release;
             printf("Enter key and release:\n");
             scanf("%u %u", &key, &release);
             delete_one_version(table, key, release);
-        } else if (strcmp(input, "da") == 0) {
+        } else if (strcmp(input, "delall") == 0) {
             KeyType key;
             printf("Enter key:\n");
             scanf("%u", &key);
             delete_all_versions(table, key);
-        } else if (strcmp(input, "sa") == 0) {
-            KeyType key;
-            printf("Enter key:\n");
-            scanf("%u", &key);
-            KeySpace *ks = search(table, key);
-            if (ks != NULL && ks->node != NULL) {
-                Node *node = ks->node;
-                printf("Key: %u, Release: %u, Info: %u\n", key, node->release, *(node->info));
-            } else {
-                printf("Key not found\n");
-            }
-        } else if (strcmp(input, "o") == 0) {
-            output(table);
-        } else if (strcmp(input, "f") == 0) {
-            char filename[100];
-            printf("Enter filename:\n");
-            scanf("%s", filename);
-            import(table, filename);
-        } else if (strcmp(input, "sa") == 0) {
+        } else if (strcmp(input, "find") == 0) {
             KeyType key;
             printf("Enter key:\n");
             scanf("%u", &key);
@@ -56,6 +36,30 @@ int main() {
                 printf("Key: %u, Release: %u, Info: %u\n", key, node->release, *(node->info));
                 node = search_all(NULL, key);
             }
+
+            // KeyType key;
+            // printf("Enter key:\n");
+            // scanf("%u", &key);
+            // KeySpace *ks = search(table, key);
+            // if (ks != NULL && ks->node != NULL) {
+            //     Node *node = ks->node;
+            //     printf("Key: %u, Release: %u, Info: %u\n", key, node->release, *(node->info));
+            // } else {
+            //     printf("Key not found\n");
+            // }
+        } else if (strcmp(input, "out") == 0) {
+            output(table);
+        } else if (strcmp(input, "file") == 0) {
+            char filename[100];
+            printf("Enter filename:\n");
+            scanf("%s", filename);
+            import(table, filename);
+        } else if (strcmp(input, "quit") == 0) {
+            run = false;
+        } else {
+            printf("\033[0;31m"); // set color to red
+            printf("Yo mama is so stpid!!! >:(\n");
+            printf("\033[0m"); // reset color to default
         }
 
         free(input);
