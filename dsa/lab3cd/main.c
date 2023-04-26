dd728f623ec2a9279971d632e037bb455c1297a4
0dminnimda <0dminnimda@gmail.com>
2023-04-26 09:39:20 +0300

    dsa/lab3cd: remove cli array and make it into only macro, rename some commands

--- dsa/lab3cd/main.c
+++ dsa/lab3cd/main.c
@@ -2,37 +2,11 @@
 #include "table.c"
 #include "common/input.h"
 
-const static char *names[] = {
-    "input",
-    "delete",
-    "deleteall",
-    "find",
-    "findspecific",
-    "output",
-    "file",
-    "quit",
-};
-
-const static char *short_names[] = {
-    "i",
-    "d",
-    "da",
-    "f",
-    "fs",
-    "o",
-    "f",
-    "q",
-};
-
-#define IS_COMMAND(input, index) (strcmp(input, names[index]) == 0 || strcmp(input, short_names[index]) == 0)
+#define IS_COMMAND(input, name, short_name) (strcmp(input, name) == 0 || strcmp(input, short_name) == 0)
 
 #define CLEAR() while ((getchar()) != '\n');
 
-#define SCAN(n, ...) if (scanf(__VA_ARGS__) != n) { \
-    printf("Error: invalid input\n"); \
-    CLEAR(); \
-    continue; \
-} CLEAR();
+#define SCAN(n, ...) if (scanf(__VA_ARGS__) != n) { CLEAR(); printf("Error: invalid input\n"); continue; } else { CLEAR(); }
 
 int main() {
     Table *table = create_table(10);
@@ -41,7 +15,7 @@ int main() {
     bool run = true;
     while (run) {
         printf("\033[32m"); // set text color to green
-        printf("Enter command (input/delete/deleteall/find/findspecific/output/file/quit):\n");
+        printf("Enter command (input/delete/deleteall/find/findall/output/file/quit):\n");
         printf("\033[0m"); // reset text color to default
         printf("> ");
 
@@ -50,24 +24,33 @@ int main() {
 
         if (input == NULL) {
             run = false;
-        } else if (IS_COMMAND(input, 0)) {
+        } else if (IS_COMMAND(input, "input", "i")) {
             KeyType key;
             Item info;
             printf("Enter key and info:\n");
             SCAN(2, "%u %u", &key, &info);
             insert(table, key, info);
-        } else if (IS_COMMAND(input, 1)) {
+        } else if (IS_COMMAND(input, "delete", "d")) {
             KeyType key;
             RelType release;
             printf("Enter key and release:\n");
             SCAN(2, "%u %u", &key, &release);
             delete_one_version(table, key, release);
-        } else if (IS_COMMAND(input, 2)) {
+        } else if (IS_COMMAND(input, "deleteall", "da")) {
             KeyType key;
             printf("Enter key:\n");
             SCAN(1, "%u", &key);
             delete_all_versions(table, key);
-        } else if (IS_COMMAND(input, 3)) {
+        } else if (IS_COMMAND(input, "find", "f")) {
+            KeyType key;
+            RelType release;
+            printf("Enter key and release:\n");
+            SCAN(2, "%u %u", &key, &release);
+            Node *node = search_spesific(table, key, release);
+            if (node != NULL) {
+                printf("Key: %u, Release: %u, Info: %u\n", key, node->release, *(node->info));
+            }
+        } else if (IS_COMMAND(input, "findall", "fa")) {
             KeyType key;
             printf("Enter key:\n");
             SCAN(1, "%u", &key);
@@ -79,23 +62,14 @@ int main() {
                 printf("Key: %u, Release: %u, Info: %u\n", key, node->release, *(node->info));
                 node = search_all(NULL, key);
             }
-        } else if (IS_COMMAND(input, 4)) {
-            KeyType key;
-            RelType release;
-            printf("Enter key and release:\n");
-            SCAN(2, "%u %u", &key, &release);
-            Node *node = search_spesific(table, key, release);
-            if (node != NULL) {
-                printf("Key: %u, Release: %u, Info: %u\n", key, node->release, *(node->info));
-            }
-        } else if (IS_COMMAND(input, 5)) {
+        } else if (IS_COMMAND(input, "output", "o")) {
             output(table);
-        } else if (IS_COMMAND(input, 6)) {
+        } else if (IS_COMMAND(input, "file", "f")) {
             char filename[100];
             printf("Enter filename:\n");
             SCAN(1, "%s", filename);
             import(table, filename);
-        } else if (IS_COMMAND(input, 7)) {
+        } else if (IS_COMMAND(input, "quit", "q")) {
             run = false;
         } else {
             printf("\033[0;31m"); // set color to red
