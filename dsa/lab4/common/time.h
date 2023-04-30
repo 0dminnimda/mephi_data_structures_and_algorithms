c5f05e11b40b3c35f4eac5e3baab0cdb7bcacc67
0dminnimda <0dminnimda@gmail.com>
2023-04-30 17:19:52 +0300

    dsa\lab4\common\time.h: format

--- dsa/lab4/common/time.h
+++ dsa/lab4/common/time.h
@@ -1,8 +1,8 @@
 long double current_time();
 
-#define TIMEIT(timing, code)              \
-    {                                     \
-        long double begin = current_time();    \
-        code;                             \
-        timing += current_time() - begin; \
+#define TIMEIT(timing, code)                \
+    {                                       \
+        long double begin = current_time(); \
+        code;                               \
+        timing += current_time() - begin;   \
     }
