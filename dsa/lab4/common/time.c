65a7af743ba0104189d8c4f9dcb816502c36f125
0dminnimda <0dminnimda@gmail.com>
2023-05-03 10:07:45 +0300

    dsa/lab4/common/time.c: add support for termux clang

--- dsa/lab4/common/time.c
+++ dsa/lab4/common/time.c
@@ -1,7 +1,13 @@
+#define _POSIX_C_SOURCE 199309L
+
 #include <time.h>
 
 long double current_time() {
-#if __STDC_VERSION__ >= 201112L  // C11
+#if (_POSIX_TIMERS > 0) && (_POSIX_C_SOURCE >= 199309L)
+    struct timespec ts;
+    if (clock_gettime(CLOCK_REALTIME, &ts) == -1) return -1;
+    return (long double)ts.tv_sec + (long double)ts.tv_nsec / 1e+9;
+#elif __STDC_VERSION__ >= 201112L  // C11
     struct timespec ts;
     timespec_get(&ts, TIME_UTC);
     return (long double)ts.tv_sec + (long double)ts.tv_nsec / 1e+9;
